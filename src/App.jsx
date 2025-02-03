46851df2bcfd5b385df8b6f0026cf5050523165d
Vishakh <vishakhm.cs@gmail.com>
2025-02-02 23:59:03 -0800

    moved aruond some files + testing out react-bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Navbar from './components/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+
+  return (
+    <>
+    <Navbar/>
+    </>
+  )
+}
+
+export default App
