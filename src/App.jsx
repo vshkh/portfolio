c84426aba812e8bfd1105ba49b05fabc190e6d1f
Vishakh <vishakhm.cs@gmail.com>
2025-02-04 10:12:39 -0800

    Customizing navbar and trying to build a hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
 
   return (
-    <>
-    <Navbar/>
-    </>
+    <Router>
+      <Navbar/>
+      <Hero/>
+      <Routes>
+      </Routes>
+    </Router>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,25 +7,15 @@ function BasicExample() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">Vishakh's Site</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
+        <Navbar.Brand href="#home">
+        <img src="https://i.imgur.com/BbGzLPy.png" 
+            width="30"  
+            height="30" 
+            className="d-inline-block align-top"
+            alt="Vishakh's Site Logo"/>
+        <span className="h3 m-2 text-pr">Vishakh's World</span>
+        </Navbar.Brand>
+        <Navbar.Collapse/>
       </Container>
     </Navbar>
   );
